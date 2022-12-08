017f76c5839d1045118b15fb5a3cc7b4a731239c
F1st3K <kostinnikita988@gmail.com>
2022-12-09 00:34:14 +0300

    add run horisontal hero

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForce = 15f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position+dir, speed*Time.deltaTime);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,7 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Horizontal"))
+            Run();
     }
 }
