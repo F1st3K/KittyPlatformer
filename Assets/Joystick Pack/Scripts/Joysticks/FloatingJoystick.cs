4479ba24e6fe794d44f81bff8f1a1269f908772a
F1st3K <kostinnikita988@gmail.com>
2022-12-15 18:39:21 +0300

    create model Joystic and attack button

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    private Vector3 Center;
+    
+    protected override void Start()
+    {
+        base.Start();
+        Center = background.anchoredPosition;
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.anchoredPosition = Center;
+        base.OnPointerUp(eventData);
+    }
+}
