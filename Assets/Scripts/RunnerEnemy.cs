9015b029a00f052898f51acd8cd4321301be7c43
F1st3K <kostinnikita988@gmail.com>
2022-12-20 20:46:20 +0300

    start change move runnerEnumy check ground

--- Assets/Scripts/RunnerEnemy.cs
+++ Assets/Scripts/RunnerEnemy.cs
@@ -6,36 +6,54 @@ using UnityEngine.UIElements;
 
 public class RunnerEnemy : Enemy
 {
-    [SerializeField] private Transform secondPosition;
+    [SerializeField] private Transform eye;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float edgeDistance;
     [SerializeField] private float speed;
-    
+
     private float _firstX;
     private float _secondX;
     private SpriteRenderer _sprite;
+    public int Direction => _sprite.flipX ? -1 : 1;
 
     private void Awake()
     {
         _sprite = GetComponentInChildren<SpriteRenderer>();
-        _firstX = transform.TransformPoint(0, 0, 0).x;
-        _secondX = secondPosition.TransformPoint(0, 0, 0).x;
+        RotateOnDirection();
     }
 
     private void Wander()
     {
-        var prevPosition = transform.position;
+        Vector3 prevPosition = transform.position;
         var postPosition = new Vector3(_secondX, prevPosition.y);
         var currentPosition = Vector3.MoveTowards(prevPosition,
                                                           postPosition,
                                                  speed*Time.deltaTime);
         transform.position = currentPosition;
     }
+
+    private void RotateOnDirection()
+    {
+        _firstX = transform.position.x;
+        _secondX = _firstX + Direction * maxDistance;
+        Vector3 eyeLocalPosition = eye.localPosition;
+        eye.localPosition = new Vector3(Math.Abs(eyeLocalPosition.x) * Direction, eyeLocalPosition.y);
+    }
     
     private void CheckWanderPosition()
     {
-        if (Math.Abs(transform.position.x - _secondX) != 0) return;
-        _secondX = _firstX;
-        _firstX = transform.position.x;
-        _sprite.flipX = !_sprite.flipX;
+        Vector3 eyePosition = eye.position;
+        RaycastHit2D rayOnGround = Physics2D.Raycast(eyePosition, Vector2.down, edgeDistance);
+        RaycastHit2D rayOnWall = Physics2D.Raycast(eyePosition, Vector2.right*Direction, edgeDistance);
+        if (Math.Abs(transform.position.x - _secondX) < edgeDistance ||
+            rayOnGround.collider == false /*||
+            rayOnWall.collider == true*/)                  
+        {
+            _sprite.flipX = !_sprite.flipX;
+            RotateOnDirection();
+        }
+
+
     }
 
     private void Update()
