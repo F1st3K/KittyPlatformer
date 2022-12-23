f5b4f57b95ad17435f6fe08e9b87a44e94753d16
F1st3K <kostinnikita988@gmail.com>
2022-12-23 22:16:28 +0300

    add load level

---
+++ Assets/Scripts/FunctionMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FunctionMenu : MonoBehaviour
+{
+    private int _indexCurrentLevel;
+
+    public void SelectLevel(int buildIndex)
+    {
+        _indexCurrentLevel = buildIndex;
+    }
+
+    public void LoadCurrentLevel()
+    {
+        SceneManager.LoadScene(_indexCurrentLevel);
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
