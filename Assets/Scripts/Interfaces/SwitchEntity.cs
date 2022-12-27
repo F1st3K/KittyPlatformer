4d931ab8245d054def49bc035ebc2650cb320891
F1st3K <kostinnikita988@gmail.com>
2022-12-27 15:00:55 +0300

    add SwitchEntity

---
+++ Assets/Scripts/Interfaces/SwitchEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interfaces
+{
+    public abstract class SwitchEntity : MonoBehaviour, ISwitching
+    {
+        [SerializeField] private bool isActivate;
+        public bool IsActivate => isActivate;
+
+        public void Activate()
+        {
+            isActivate = true;
+            OnActivate();
+        }
+
+        public void DeActivate()
+        {
+            isActivate = false;
+            OnDeActivate();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.TryGetComponent(out IStateChanger changer) &&
+                changer.SwitchingEntity is null)
+                changer.SetSwitchingEntity(this);
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.TryGetComponent(out IStateChanger changer) &&
+                changer.SwitchingEntity is not null)
+                changer.SetSwitchingEntity(null);
+        }
+
+        private protected virtual void OnActivate()
+        {
+        
+        }
+        
+        private protected virtual void OnDeActivate()
+        {
+        
+        }
+    }
+}
--- Assets/Scripts/Interfaces/Сharacter.cs
+++ Assets/Scripts/Interfaces/Сharacter.cs
@@ -2,15 +2,28 @@
 
 namespace Interfaces
 {
-    public sealed class Сharacter : LivingEntity, IAttacking
+    public sealed class Сharacter : LivingEntity, IAttacking, IStateChanger
     {
         [SerializeField] private Weapon weapon;
         
         public IWeaponer Weapon => weapon;
+        public ISwitching SwitchingEntity { get; private set; }
         
         public void Attack(Vector2 direction, float power)
         {
             weapon.Fire(direction, power);
+        }
+
+        public void SetSwitchingEntity(ISwitching obj)
+        {
+            SwitchingEntity = obj;
+        }
+
+        public void ToggleCurrentSwitchingEntity()
+        {
+            if (SwitchingEntity.IsActivate)
+                SwitchingEntity.DeActivate();
+            else SwitchingEntity.Activate();
         }
     }
 }
