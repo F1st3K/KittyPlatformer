7a6e63f70cbb6276a96951aa6f46343cd4ba63e9
F1st3K <kostinnikita988@gmail.com>
2022-12-27 14:10:34 +0300

    add IStateChanger

---
+++ Assets/Scripts/Interfaces/IStateChanger.cs
@@ -0,0 +1,12 @@
+﻿using Unity.VisualScripting;
+
+namespace Interfaces
+{
+    public interface IStateChanger
+    {
+        public ISwitching SwitchingEntity { get; }
+
+        public void SetSwitchingEntity(ISwitching obj);
+        public void ToggleCurrentSwitchingEntity();
+    }
+}
