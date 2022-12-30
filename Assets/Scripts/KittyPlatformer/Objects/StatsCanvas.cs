4c5dc6f90918445ab6d2e7438041e5ff829ae659
F1st3K <kostinnikita988@gmail.com>
2022-12-30 21:55:56 +0300

    add stats Canvas(health Bar)

---
+++ Assets/Scripts/KittyPlatformer/Objects/StatsCanvas.cs
@@ -0,0 +1,22 @@
+﻿using KittyPlatformer.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KittyPlatformer.Objects
+{
+    public class StatsCanvas : MonoBehaviour
+    {
+        [SerializeField] private LivingEntity entity;
+        [SerializeField] private Image fillHealthBar;
+
+        private void Awake()
+        {
+            (entity as ILiving).HealthChanged += UpdateHealthBar;
+        }
+
+        private void UpdateHealthBar()
+        {
+            fillHealthBar.fillAmount = (float)entity.HealthPoint / entity.MaxHealthPoint;
+        }
+    }
+}
