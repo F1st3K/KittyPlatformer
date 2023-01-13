83464fcde9449145781a2193c579cdce33f5adb0
F1st3K <kostinnikita988@gmail.com>
2023-01-13 08:04:26 +0300

    create Health Vial

---
+++ Assets/Scripts/KittyPlatformer/Objects/HealthVial.cs
@@ -0,0 +1,21 @@
+﻿using KittyPlatformer.Base;
+using KittyPlatformer.Interfaces;
+using UnityEngine;
+
+namespace KittyPlatformer.Objects
+{
+    public sealed class HealthVial : StateVariableEntity
+    {
+        [SerializeField] private int countHealth;
+        
+        private protected override void OnActivate()
+        {
+            if (Entity is ILiving liver)
+            {
+                liver.GetHealth(countHealth);
+                Destroy(gameObject);
+            }
+            DeActivate();
+        }
+    }
+}
